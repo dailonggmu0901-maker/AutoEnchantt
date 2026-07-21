2813c071b87e2fedce075b48455a83859da1ff63
dailonggmu0901-maker <dailonggmu0901@gmail.com>
2026-07-21 08:25:09 +0700

    Update AutoEnchant.java

--- src/main/java/me/hilo/autoenchant/AutoEnchant.java
+++ src/main/java/me/hilo/autoenchant/AutoEnchant.java
@@ -1,6 +1,5 @@
 package me.hilo.autoenchant;
 
-import meteordevelopment.meteorclient.MeteorClient;
 import net.fabricmc.api.ClientModInitializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -8,27 +7,12 @@ import org.slf4j.LoggerFactory;
 public class AutoEnchant implements ClientModInitializer {
 
     public static final String MOD_ID = "autoenchant";
-    public static final Logger LOG = LoggerFactory.getLogger(MOD_ID);
+    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
     @Override
     public void onInitializeClient() {
-
-        LOG.info("=================================");
-        LOG.info(" AutoEnchant Loaded!");
-        LOG.info(" Minecraft 1.21.4");
-        LOG.info("=================================");
-
-        // Đăng ký command
-        // CommandManager.register();
-
-        // Khởi tạo scanner
-        // BlockScanner.init();
-
-        // Khởi tạo Baritone
-        // BaritoneManager.init();
-
-        // Khởi tạo EnchantManager
-        // EnchantManager.init();
-
+        LOGGER.info("==============================");
+        LOGGER.info(" AutoEnchant Loaded!");
+        LOGGER.info("==============================");
     }
 }
